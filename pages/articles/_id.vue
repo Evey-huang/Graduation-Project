340fe9f4a4820af4c4714cc66677fa489476fa56
Evey-huang <Evey-huang@evey_wong@163.com>
2018-04-02 19:28:39 +0800

    博客静态页面

---
+++ pages/articles/_id.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="article">this is articles page</div>
+</template>
+
+<script>
+export default {
+  layout: 'default'
+}
+</script>
--- pages/blog.vue
+++ pages/blog.vue
@@ -105,14 +105,32 @@
           </div>
           <div class="aside">
               <div class="aside-article">
-                  <!-- <p class="title"><span></span></p>
+                  <p class="title"><span>最热文章</span></p>
                   <ul>
-                      <li></li>
-                      <li></li>
-                      <li></li>
-                  </ul> -->
+                      <li>
+                          <span><nuxt-link to="">实现基于Nuxt.js的SSR应用</nuxt-link></span>
+                          <p>sky-cloud前端团队</p>
+                      </li>
+                      <li>
+                          <span><nuxt-link to="#">实现基于Nuxt.js的SSR应用</nuxt-link></span>
+                          <p>sky-cloud前端团队</p>
+                      </li>
+                      <li>
+                          <span><nuxt-link to="#">实现基于Nuxt.js的SSR应用</nuxt-link></span>
+                          <p>sky-cloud前端团队</p>
+                      </li>
+                  </ul>
               </div>
-              <div class="aside-tag"></div>
+              <div class="aside-tag">
+                  <p class="title"><span>标签</span></p>
+                  <ul>
+                      <li v-for="item in data" v-bind:key="item.key">
+                          <nuxt-link to="">
+                              <span v-html="item.text" :style="{'font-size': item.num>150?'36px':item.num>100?'28px':item.num>50?'20px':item.num>20?'18px':'14px'}"></span>
+                          </nuxt-link>
+                      </li>
+                  </ul>
+              </div>
           </div>
       </div>
   </div>
@@ -120,18 +138,35 @@
 
 <script>
 export default {
-  layout: 'default'
+  layout: 'default',
+  data(){
+      return {
+          data: [
+              {text: "node.js",num: 10,key: 1},
+              {text: "MongoDB",num: 20,key: 2},
+              {text: "云安全",num: 30,key: 3},
+              {text: "运维",num: 40,key: 4},
+              {text: "数据库",num: 70,key: 5},
+              {text: "新产品发布",num: 50,key: 6},
+              {text: "Docker",num: 150,key: 7}
+          ]
+      }
+  }
 }
 </script>
 
 <style lang="scss" scoped>
+@import '~assets/scss/mixins.scss';
+
   .blog {
     margin-top: 120px;
     margin-left: 80px;
     .container {
+        @include clearfix;
         .article-list {
-            width: 60%;
-            // background: #666;
+            min-width: 60%;
+            float: left;
+            margin-left: 40px;
             .article-list-item {
                 border-bottom: 1px solid #DDDFE2;
                 .item-content {
@@ -167,11 +202,59 @@ export default {
             }
         }
         .aside {
-            .aside-article {
-
+            float: right;
+            margin-top: 50px;
+            margin-right: 40px;
+            width: 20%;
+            .title {
+                border-bottom: 1px solid #DDDFE2;
+                padding-bottom: 20px; 
+                width: 100%;
+                span {
+                    border-left: 3px solid #4A90E2;
+                    padding-left: 10px;
+                    font-size: 20px;
+                    color: #666;
+                }
+            }
+            .aside-article {  
+                ul {
+                    margin-top: 20px;
+                    li {
+                        line-height: 1.5;
+                        margin-bottom: 15px;
+                        span {
+                            a {
+                                color: #212121;
+                                font-size: 16px;
+                            }
+                        }
+                        p {
+                            color: #999;
+                            font-size: 14px;
+                        }
+                    }
+                }
             }
             .aside-tag {
-
+                margin-top: 50px;
+                ul {
+                    margin-top: 20px;
+                    display: flex;
+                    flex-wrap: wrap;
+                    align-items: center;
+                    li {
+                        display: inline-block;
+                        background: #F7F7F7;
+                        border: 1px solid #DDDFE2;
+                        margin: 5px;
+                        a {
+                            color: #666;
+                            padding: 15px;
+                            display: inline-block;
+                        }
+                    }
+                }
             }
         }
     }
