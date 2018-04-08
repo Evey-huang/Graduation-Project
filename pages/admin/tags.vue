7a2538df6f3db5b0ef5d7941d6fd5d94081aefe5
Evey-huang <Evey-huang@evey_wong@163.com>
2018-04-08 19:27:39 +0800

    标签页面

--- pages/admin/tags.vue
+++ pages/admin/tags.vue
@@ -3,24 +3,24 @@
     <div class="header">
       <p class="title">标签管理</p>
       <el-input class="search" placeholder="请输入内容"></el-input>
-      <span @click="tagDialog = true"><i class="icon el-icon-plus"></i></span>
+      <span @click="handleNew"><i class="icon el-icon-plus"></i></span>
       <span><i class="icon el-icon-refresh"></i></span>
-      <el-dialog title="添加标签" :visible.sync="tagDialog" width="20%" :before-close="handleClose" center>
-        <el-form :model="form">
+      <el-dialog title="添加标签" :visible.sync="tagDialog.isShow" width="20%" center>
+        <el-form :model="tags">
           <el-form-item label="名称">
-            <el-input v-model="form.name"></el-input>
+            <el-input v-model="tags.name" placeholder="请输入标签名称"></el-input>
           </el-form-item>
           <el-form-item label="描述">
-            <el-input v-model="form.name"></el-input>
+            <el-input v-model="tags.description" placeholder="请输入标签描述"></el-input>
           </el-form-item>
         </el-form>
         <div slot="footer" class="dialog-footer">
-          <el-button @click="tagDialog = false">取 消</el-button>
-          <el-button type="primary" @click="tagDialog = false">确 定</el-button>
+          <el-button @click="closeTagDialog">取 消</el-button>
+          <el-button type="primary" @click="commitUpdateTag">确 定</el-button>
         </div>
       </el-dialog>
     </div>
-    <el-table :data="tableData3" tooltip-effect="dark" style="width: 90%" @selection-change="handleSelectionChange">
+    <el-table :data="tagData" tooltip-effect="dark" style="width: 90%" @selection-change="handleSelectionChange">
       <el-table-column width="55"></el-table-column>
       <el-table-column label="名称" width="120">
         <template slot-scope="scope">{{ scope.row.tag }}</template>
@@ -29,8 +29,8 @@
       <el-table-column prop="number" label="文章" show-overflow-tooltip sortable></el-table-column>
       <el-table-column label="操作">
         <template slot-scope="scope">
-          <el-button size="mini" @click="handleEdit(scope.$index, scope.row)">编辑</el-button>
-          <el-button size="mini" type="danger" @click="handleDelete(scope.$index, scope.row)">删除</el-button>
+          <el-button size="mini" @click="handleEdit">编辑</el-button>
+          <el-button size="mini" type="danger" @click="handleDelete">删除</el-button>
         </template>
       </el-table-column>
     </el-table>
@@ -41,8 +41,7 @@
 export default {
   data() {
     return {
-      tagDialog: false,
-      tableData3: [{
+      tagData: [{
         tag: 'Node.js',
         number: 11,
         description: '描述描述描述'
@@ -72,28 +71,53 @@ export default {
         description: '描述描述描述'
       }],
       multipleSelection: [],
-      form: {
-          name: '',
-          region: '',
-          date1: '',
-          date2: '',
-          delivery: false,
-          type: [],
-          resource: '',
-          desc: ''
-        }
+      tags: { // 标签信息
+        name: '',
+        description: ''
+      },
+      tagDialog: { // 标签弹窗
+        isShow: false,
+        add: false,
+        edit: false
+      }
     }
   },
   methods: {
     handleSelectionChange(val) {
       this.multipleSelection = val;
     },
-    handleClose(done) {
-      this.$confirm('确认关闭？')
-        .then(_ => {
-          done();
-        })
-        .catch(_ => {});
+    // 关闭Dialog窗口
+    closeTagDialog() {
+      this.tagDialog = {
+        add: false,
+        edit: false,
+        isShow: false
+      }
+    },
+    // 添加标签
+    handleNew() {
+      this.closeTagDialog()
+      this.tags = {
+        name: '',
+        description: ''
+      }
+      this.tagDialog.add = true
+      this.tagDialog.isShow = true
+    },
+    // 编辑修改标签
+    handleEdit() {
+
+    },
+    // 删除标签
+    handleDelete() {
+
+    },
+    // 提交
+    commitUpdateTag() {
+      let params = {
+        name: this.tags.name,
+        description: this.tags.description
+      }
     }
   }
 }
